0d58866b08fc228bea59c9a6e5b1dc2adf959de5
Lizardo Perdomo <lizardo@utp.edu.co>
2026-05-22 10:56:32 -0500

    :white_check_mark: Added transfer services tests

--- src/__mocks__/config.ts
+++ src/__mocks__/config.ts
@@ -2,5 +2,7 @@ export const config = {
   apiURLs: {
     auth: 'http://localhost:3000',
     balance: 'http://localhost:3001',
+    transfer: 'http://localhost:3002',
+    transferList: 'http://localhost:3003',
   },
 };

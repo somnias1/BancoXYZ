09fc5c5cd9c6e6a25683551cdb3ae3bd915c5b20
Lizardo Perdomo <lizardo@utp.edu.co>
2026-05-22 10:43:30 -0500

    :alien: Added transfer services

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
 export const config = {
-    apiURLs: {
-        auth: import.meta.env.VITE_API_URL_AUTH,
-        balance: import.meta.env.VITE_API_URL_BALANCE,
-    }
-}
+  apiURLs: {
+    auth: import.meta.env.VITE_API_URL_AUTH,
+    balance: import.meta.env.VITE_API_URL_BALANCE,
+    transfer: import.meta.env.VITE_API_URL_TRANSFER,
+    transferList: import.meta.env.VITE_API_URL_TRANSFER_LIST,
+  },
+};
